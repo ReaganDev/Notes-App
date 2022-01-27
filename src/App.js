9c7736968d3aa193c93be73ab2a175c679d4d25a
Reagan Reuben <reagan.reuben@decagon.dev>
2022-01-27 13:20:40 +0100

    icons,useStyles hook, custom themes and textfields

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Create from './pages/Create'
 import Notes from './pages/Notes'
+import { createTheme, ThemeProvider } from '@material-ui/core'
+import { purple } from '@material-ui/core/colors'
+
+const theme = createTheme({
+	palette: {
+		primary: purple,
+	},
+})
 
 function App() {
 	return (
-		<Router>
-			<Routes>
-				<Route exact path='/' element={<Notes />} />
-				<Route path='/create' element={<Create />} />
-			</Routes>
-		</Router>
+		<ThemeProvider theme={theme}>
+			<Router>
+				<Routes>
+					<Route exact path='/' element={<Notes />} />
+					<Route path='/create' element={<Create />} />
+				</Routes>
+			</Router>
+		</ThemeProvider>
 	)
 }
 
--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,8 +1,40 @@
 import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
+import Container from '@material-ui/core/Container'
+import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight'
+import { makeStyles } from '@material-ui/core'
+import TextField from '@material-ui/core/TextField'
+import { useRef, useState } from 'react'
+
+const useStyles = makeStyles({
+	field: {
+		marginBottom: 20,
+		marginTop: 20,
+		display: 'block',
+	},
+})
 
 const Create = () => {
+	const titleRef = useRef()
+	const detailsRef = useRef()
+	const classes = useStyles()
+	const [detailsError, setDetailsError] = useState(false)
+	const [titleError, setTitleError] = useState(false)
+	function handleSubmit(e) {
+		e.preventDefault()
+		setDetailsError(false)
+		setTitleError(false)
+		if (titleRef.current.value === '') {
+			setTitleError(true)
+		}
+		if (detailsRef.current.value === '') {
+			setDetailsError(true)
+		} else if (titleRef.current.value && detailsRef.current.value) {
+			console.log(titleRef.current.value, detailsRef.current.value)
+		}
+	}
 	return (
-		<div>
+		<Container>
 			<Typography
 				variant='h6'
 				component='h2'
@@ -11,7 +43,40 @@ const Create = () => {
 			>
 				Create a New Note
 			</Typography>
-		</div>
+
+			<form onSubmit={handleSubmit} noValidate autoComplete='off'>
+				<TextField
+					className={classes.field}
+					color='primary'
+					label='Note Title'
+					variant='outlined'
+					fullWidth
+					inputRef={titleRef}
+					required
+					error={titleError}
+				/>
+				<TextField
+					className={classes.field}
+					color='primary'
+					label='Details'
+					multiline
+					error={detailsError}
+					rows={4}
+					variant='outlined'
+					fullWidth
+					inputRef={detailsRef}
+					required
+				/>
+				<Button
+					color='primary'
+					variant='contained'
+					type='submit'
+					endIcon={<KeyboardArrowRightIcon />}
+				>
+					Submit
+				</Button>
+			</form>
+		</Container>
 	)
 }
 
