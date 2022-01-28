05db9b46f465057ff1d5739812f9431a69e209c7
Reagan Reuben <reagan.reuben@decagon.dev>
2022-01-28 21:26:52 +0100

    using avatars

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,34 +1,51 @@
 import {
+	AppBar,
+	Avatar,
 	Drawer,
 	List,
 	ListItem,
 	ListItemIcon,
 	ListItemText,
 	makeStyles,
+	Toolbar,
 	Typography,
 } from '@material-ui/core'
 import { AddCircleOutlined, SubjectOutlined } from '@material-ui/icons'
+import { format } from 'date-fns'
 import { useLocation, useNavigate } from 'react-router-dom'
 
 const drawerWidth = 240
 
-const useStyles = makeStyles({
-	page: {
-		backgroundColor: '#f9f9f9',
-		width: '100%',
-	},
-	drawer: {
-		width: drawerWidth,
-	},
-	drawerPaper: {
-		width: drawerWidth,
-	},
-	root: {
-		display: 'flex',
-	},
-	active: {
-		backgroundColor: '#f4f4f4',
-	},
+const useStyles = makeStyles((theme) => {
+	return {
+		page: {
+			backgroundColor: '#f9f9f9',
+			width: '100%',
+			padding: theme.spacing(3),
+		},
+		drawer: {
+			width: drawerWidth,
+		},
+		drawerPaper: {
+			width: drawerWidth,
+		},
+		root: {
+			display: 'flex',
+		},
+		active: {
+			backgroundColor: '#f4f4f4',
+		},
+		title: {
+			padding: theme.spacing(2),
+		},
+		appbar: {
+			width: `calc(100% - ${drawerWidth}px)`,
+		},
+		toolbar: theme.mixins.toolbar,
+		date: {
+			flexGrow: 1,
+		},
+	}
 })
 
 const Layout = ({ children }) => {
@@ -49,6 +66,15 @@ const Layout = ({ children }) => {
 	]
 	return (
 		<div className={classes.root}>
+			<AppBar className={classes.appbar} color='transparent' elevation={0}>
+				<Toolbar>
+					<Typography className={classes.date} variant='h5'>
+						Today is the {format(new Date(), 'do MMMM Y')}
+					</Typography>
+					<Typography>Reagan</Typography>
+					<Avatar />
+				</Toolbar>
+			</AppBar>
 			<Drawer
 				className={classes.drawer}
 				variant='permanent'
@@ -56,7 +82,9 @@ const Layout = ({ children }) => {
 				classes={{ paper: classes.drawerPaper }}
 			>
 				<div>
-					<Typography variant='h5'>Notes App</Typography>
+					<Typography className={classes.title} variant='h5'>
+						Notes App
+					</Typography>
 				</div>
 
 				<List>
@@ -65,7 +93,9 @@ const Layout = ({ children }) => {
 							onClick={() => navigate(menu.path)}
 							button
 							key={menu.text}
-							className={location.pathname === menu.path && classes.active}
+							className={
+								location.pathname === menu.path ? classes.active : null
+							}
 						>
 							<ListItemIcon>{menu.icon}</ListItemIcon>
 							<ListItemText primary={menu.text} />
@@ -73,7 +103,10 @@ const Layout = ({ children }) => {
 					))}
 				</List>
 			</Drawer>
-			<div className={classes.page}>{children}</div>
+			<div className={classes.page}>
+				<div className={classes.toolbar}></div>
+				{children}
+			</div>
 		</div>
 	)
 }
--- src/components/NotesCard.jsx
+++ src/components/NotesCard.jsx
@@ -1,17 +1,41 @@
 import {
+	Avatar,
 	Card,
 	CardContent,
 	CardHeader,
 	IconButton,
+	makeStyles,
 	Typography,
 } from '@material-ui/core'
+import { blue, green, pink, yellow } from '@material-ui/core/colors'
 import { DeleteOutlined } from '@material-ui/icons'
 
+const useStyles = makeStyles({
+	avatar: {
+		backgroundColor: (note) => {
+			if (note.category === 'Work') {
+				return yellow[700]
+			}
+			if (note.category === 'Money') {
+				return green[500]
+			}
+			if (note.category === 'Todos') {
+				return pink[500]
+			}
+			return blue[500]
+		},
+	},
+})
+
 const NotesCard = ({ note, deleteNote }) => {
+	const classes = useStyles(note)
 	return (
 		<div>
 			<Card elevation={1}>
 				<CardHeader
+					avatar={
+						<Avatar className={classes.avatar}>{note.category[0]}</Avatar>
+					}
 					action={
 						<IconButton onClick={() => deleteNote(note.id)}>
 							<DeleteOutlined />
